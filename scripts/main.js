091fa022ae0c7f763eddc5bbfa0ae7ae87011072
Gordon Zhang <gordon-zhang@hotmail.com>
2021-06-20 16:48:17 -0400

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/me.jpg') {
+      myImage.setAttribute('src','images/me2.jpg');
+    } else {
+      myImage.setAttribute('src','images/me.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+}
+
+myButton.onclick = function() {
+    setUserName();
+  }
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
